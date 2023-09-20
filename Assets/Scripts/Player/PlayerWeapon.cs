3462e528ff10e47d769d77fe947790c700ba0be6
Arkadiusz Bergmann <arkadiuszbergmann@gmail.com>
2023-09-20 15:43:00 +0200

    Using and switching weapons (WIP)

---
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using Weapons;
+
+namespace Player
+{
+    public class PlayerWeapon : MonoBehaviour
+    {
+        [SerializeField] private WeaponType defaultWeapon;
+
+        private WeaponSwitching weaponSwitching = new WeaponSwitching();
+
+        private void Start()
+        {
+            
+        }
+
+        private void OnEnable()
+        {
+            
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+
+        private void UseWeapon()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Player/WeaponSwitching.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Weapons;
+
+namespace Player
+{
+    public class WeaponSwitching
+    {
+        private GameObject currentWeaponModel;
+        private WeaponType currentWeaponData;
+
+        private Dictionary<WeaponType, GameObject> weaponModelPool = new Dictionary<WeaponType, GameObject>();
+
+        public void SwitchToWeapon(WeaponType weaponData)
+        {
+            UnequipCurrentWeapon();
+        }
+
+        private void UnequipCurrentWeapon()
+        {
+            if (currentWeaponData == null)
+            {
+                if (currentWeaponModel != null)
+                {
+                    Object.Destroy(currentWeaponModel);
+                }
+
+                return;
+            }
+
+            weaponModelPool.Add(currentWeaponData, currentWeaponModel);
+            currentWeaponModel.SetActive(false);
+            currentWeaponModel = null;
+            currentWeaponData = null;
+        }
+
+        private void EquipWeapon(WeaponType weaponData)
+        {
+            if (weaponModelPool.TryGetValue(weaponData, out GameObject weaponModel))
+            {
+                weaponModel.SetActive(true);
+                return;
+            }
+
+            //todo handle new model instantiation
+        }
+    }
+}
